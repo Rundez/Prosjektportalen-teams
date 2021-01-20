81d23926cc907761b6d18fb6008de1ad224713da
Martin Ruud <m.ruud93@gmail.com>
2021-01-20 17:22:50 +0100

    Migrating riskmatrix from PP

---
+++ src/webparts/teams/components/RiskMatrix/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { IRiskMatrixProps } from './types'
+import { MatrixRows } from './MatrixRow'
+import styles from './RiskMatrix.module.scss'
+
+export const RiskMatrix: React.FunctionComponent<IRiskMatrixProps> = ({
+  items = [],
+  width = 400,
+  height = 300,
+  calloutTemplate
+}: IRiskMatrixProps) => {
+  return (
+    <div className={styles.riskMatrix} style={{ width, height }}>
+      <table className={styles.table}>
+        <tbody>
+          <MatrixRows items={items} calloutTemplate={calloutTemplate} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export * from './types'
