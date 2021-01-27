334ddf91699630f7fc5a1d165ae673a52a2ad091
Martin Ruud <m.ruud93@gmail.com>
2021-01-27 14:34:15 +0100

    discord web hook test

--- src/webparts/teams/components/TeamMembers/index.tsx
+++ src/webparts/teams/components/TeamMembers/index.tsx
@@ -1,5 +1,13 @@
+import { Header } from '@fluentui/react-northstar';
 import React from 'react';
+import { sp } from "@pnp/sp";
 
-const TeamMembers = () => {
-    
+export const TeamMembers = () => {
+
+    console.log(sp.site)
+
+    return (
+        <p>Team members will be shown here..</p>
+        
+    )
 }
