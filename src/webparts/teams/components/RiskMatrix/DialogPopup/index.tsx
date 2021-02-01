d9bc4cbfd1eb0c2bd8516cefa7d704a92b32eb07
Martin Ruud <m.ruud93@gmail.com>
2021-02-01 14:25:19 +0100

    testing useHook form

--- src/webparts/teams/components/RiskMatrix/DialogPopup/index.tsx
+++ src/webparts/teams/components/RiskMatrix/DialogPopup/index.tsx
@@ -7,143 +7,51 @@ import { useForm } from 'react-hook-form';
 
 
 export const AddElementDialog: FunctionComponent = () => {
-    const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
-    const [formData, setFormData] = useState([{}]);
-    const { register, handleSubmit } = useForm();
+  const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
+  const { register, handleSubmit } = useForm();
 
-    const fields = [
-        {
-          label: 'Tittel',
-          name: 'title',
-          id: 'title',
-          key: 'title',
-          required: true,
-          ref: register,
-          control: {
-            as: Input,
-            showSuccessIndicator: false,
-            fluid: true
-          },
-        },
-        {
-          label: 'Fase',
-          name: 'fase',
-          id: 'fase-shorthand',
-          key: 'fase',
-          required: false,
-          ref: register,
-          control: {
-            as: Dropdown,
-            items: phase,
-            search: true,
-            fluid: true
-          },
-        },
-        {
-            label: 'Sannsynlighet (S)',
-            name: 'propability',
-            id: 'probability',
-            key: 'probability',
-            required: true,
-            ref: register,
-            control: {
-              as: Input,
-              showSuccessIndicator: false,
-              fluid: true
-            },
-          },
-          {
-            label: 'Konsekvens',
-            name: 'consequence',
-            id: 'consequence',
-            key: 'consequence',
-            required: true,
-            ref: register,
-            control: {
-              as: Input,
-              showSuccessIndicator: false,
-              fluid: true
-            },
-          },
-          {
-            label: 'S etter tiltak',
-            name: 'probAfter',
-            id: 'probAfter',
-            key: 'probAfter',
-            required: true,
-            ref: register,
-            control: {
-              as: Input,
-              showSuccessIndicator: false,
-              fluid: true
-            },
-          },
-          {
-            label: 'K etter tiltak',
-            name: 'consAfter',
-            id: 'consAfter',
-            key: 'consAfter',
-            required: true,
-            ref: register,
-            control: {
-              as: Input,
-              showSuccessIndicator: false,
-              fluid: true
-            },
-          },
-          {
-          control: {
-            as: Button,
-            content: 'Submit',
-          },
-          key: 'submit',
-        },
-      ]
-
-      const onSubmit = (data) => {
-        console.log(data);
-      }
+  const printData = data => console.log(data);
 
 
-    return (
-        <>
-            <Button primary content="Add item" onClick={openPanel} style={{marginBottom: 10}}/>
-            <Panel
-                headerText="Add risk/opportunity"
-                isOpen={isOpen}
-                onDismiss={dismissPanel}
-                closeButtonAriaLabel="Close"
-            >
-                <div>
-                    <Form 
-                    fields={fields}
-                    onSubmit={handleSubmit(onSubmit)}
-                    />
-                    <Flex gap="gap.medium" style={{marginTop: 10}}>
-                        <FlexItem>
-                            <Button primary content="Add item" onClick={() => {dismissPanel(), addToList()}} />
-                        </FlexItem>
-                        <FlexItem>
-                            <Button secondary content="Cancel" onClick={dismissPanel} />
-                        </FlexItem>
-                    </Flex>
-                </div>
-            </Panel>
-        </>
-    )
+  return (
+    <>
+      <Button primary content="Add item" onClick={openPanel} style={{ marginBottom: 10 }} />
+      <Panel
+        headerText="Add risk/opportunity"
+        isOpen={isOpen}
+        onDismiss={dismissPanel}
+        closeButtonAriaLabel="Close"
+      >
+        <div>
+          <Form onSubmit={handleSubmit(printData)}>
+            <Input ref={register} name="title" label="Tittel" fluid/>
+            <Dropdown items={phase} fluid/>
+            <Input ref={register} name="probability" label="Sannsynlighet (S)" fluid/>
+            <Input ref={register} name="conseqence" label="Konsekvens (K)" fluid/>
+            <Input ref={register} name="propbAfter" label="S etter tiltak" fluid/>
+            <Input ref={register} name="consAfter" label="K etter tiltak" fluid/>
+            <Flex gap="gap.medium" style={{ marginTop: 10 }}>
+              <FlexItem>
+                <Button primary content="Add item" type="submit" />
+              </FlexItem>
+              <FlexItem>
+                <Button secondary content="Cancel" onClick={dismissPanel} />
+              </FlexItem>
+            </Flex>
+          </Form>
+        </div>
+      </Panel>
+    </>
+  )
 }
 
-const addToList = () => {
-    console.log("Nice");
-}
 
 const phase = [
-    'Ingen fase',
-    'Flere faser',
-    'Konsept',
-    'Planlegge',
-    'Gjennomføre',
-    'Avslutte',
-    'Realisere'
+  'Ingen fase',
+  'Flere faser',
+  'Konsept',
+  'Planlegge',
+  'Gjennomføre',
+  'Avslutte',
+  'Realisere'
 ];
-
