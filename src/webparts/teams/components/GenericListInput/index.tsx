fb41d05c9778521f7f3ec4c832ed3c0527a21d1b
Martin Ruud <m.ruud93@gmail.com>
2021-02-05 18:13:08 +0100

    Some fields, and content types fetched

--- src/webparts/teams/components/GenericListInput/index.tsx
+++ src/webparts/teams/components/GenericListInput/index.tsx
@@ -1,19 +1,37 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { IGenericListInputProps } from './types';
+import { Button } from '@fluentui/react-northstar';
 
-import { sp, List, IList, ContentTypes, IContentTypes, Fields, IFields} from "@pnp/sp/presets/all";
+import { sp, List, IList, ContentTypes, IContentTypes, Fields, IFields } from "@pnp/sp/presets/all";
 
 export const GenericListInput: FunctionComponent<IGenericListInputProps> = ({ listName }) => {
 
-    const [data, setData] = useState();
+    const [fields, setFields] = useState([]);
+    const [contentTypeID, setContentTypeID] = useState([]);
+
 
     useEffect(() => {
-       fetchListData(listName);
+        fetchViewListData(listName);
+        fetchFieldData(listName);
     }, [])
 
-    const items = [
-    ]
+    // Fetch the standard view fields
+    const fetchViewListData = async (listName) => {
+        const list = sp.web.lists.getByTitle(listName)
+            .views.getByTitle("Alle elementer")
+            .fields.get()
+            .then((data) => setFields(data["Items"]));
+        return list;
+    }
 
+    // Fetches the content type ID for the list 
+    const fetchFieldData = async (listName) => {
+        const list = sp.web.lists.getByTitle(listName)
+            .contentTypes.get()
+            .then(ct => ct.map(ct => setContentTypeID(current => [...current, ct.Id])))
+    }
+    console.log(contentTypeID);
+    console.log(fields);
     return (
         <>
         </>
@@ -22,12 +40,5 @@ export const GenericListInput: FunctionComponent<IGenericListInputProps> = ({ li
 
 const _getSelection = (items: any[]) => {
     console.log('Selected items:', items);
-  }
-
-  // Fetch the standard view fields
-const fetchListData = async (listName) => {
-    const list = sp.web.lists.getByTitle(listName)
-    .views.getByTitle("Alle elementer")
-    .fields.get()
-    .then((data) => console.log(data["Items"]))
 }
+
