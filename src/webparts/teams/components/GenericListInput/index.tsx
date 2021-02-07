8918675c93a6432ee159424053ef4dd10c8bfca0
Martin Ruud <m.ruud93@gmail.com>
2021-02-07 11:14:40 +0100

    Added query to get all column for a selected list

--- src/webparts/teams/components/GenericListInput/index.tsx
+++ src/webparts/teams/components/GenericListInput/index.tsx
@@ -8,11 +8,14 @@ export const GenericListInput: FunctionComponent<IGenericListInputProps> = ({ li
 
     const [fields, setFields] = useState([]);
     const [contentTypeID, setContentTypeID] = useState([]);
+    const [listFields, setListFields] = useState([]);
+
 
 
     useEffect(() => {
         fetchViewListData(listName);
         fetchFieldData(listName);
+        fetchListFields(listName);
     }, [])
 
     // Fetch the standard view fields
@@ -24,6 +27,14 @@ export const GenericListInput: FunctionComponent<IGenericListInputProps> = ({ li
         return list;
     }
 
+    // This should fetch fields for THE SELECTED list. AND IT DOES. Needs more testing.
+    const fetchListFields = async (listName) => {
+        const list = sp.web.lists.getByTitle(listName)
+            .fields.filter("ReadOnlyField eq false and Hidden eq false").get()
+            .then((fields) => fields.map((field) => console.log(field.Title)))
+        return list;
+    }
+
     // Fetches the content type ID for the list 
     const fetchFieldData = async (listName) => {
         const list = sp.web.lists.getByTitle(listName)
@@ -32,6 +43,7 @@ export const GenericListInput: FunctionComponent<IGenericListInputProps> = ({ li
     }
     console.log(contentTypeID);
     console.log(fields);
+
     return (
         <>
         </>
