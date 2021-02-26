472ce4fa8827f3f27a37ef5e19ee53cb90f24307
Skiip <kristian.rein@bjurholt.no>
2021-02-26 01:24:49 +0100

    "Made changes to the navbar so that its ready to take in a taxonomy to make it dynamic.

--- src/webparts/teams/components/Navigation/index.tsx
+++ src/webparts/teams/components/Navigation/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Flex, Divider } from "@fluentui/react-northstar";
-import { Menu } from "./Menu";
+import { IMenuProps } from "./Menu/types";
+import { Menu } from "./Menu/Menu";
 
 export default function Navigation() {
   const [active, setActive] = React.useState("Home");
@@ -9,53 +10,37 @@ export default function Navigation() {
     setActive(name);
   };
 
+  const menu = [
+    {
+      path: "/",
+      name: "Home",
+      image:
+        "https://puzzlepart.com/wp-content/uploads/2019/12/Pzl-web-logo-dark-single.png",
+    },
+    { path: "/", name: "Home" },
+    { path: "/riskmatrix", name: "Usikkerhet" },
+    { path: "/projectstatus", name: "Prosjektstatus" },
+    { path: "/", name: "Page 3" },
+    { path: "/", name: "Page 4" },
+    { path: "/", name: "Page 5" },
+  ];
+
+  function setMenu(menu) {
+    return menu.map((menu) => (
+      <Menu
+        active={() => handleClick(menu.name)}
+        activeState={active}
+        path={menu.path}
+        image={menu.image}
+        name={menu.name}
+      ></Menu>
+    ));
+  }
+
   return (
     <>
       <div style={style}>
-        <Flex space="between">
-          <Menu
-            active={() => handleClick("Home")}
-            activeState={active}
-            path="/"
-            image="https://puzzlepart.com/wp-content/uploads/2019/12/Pzl-web-logo-dark-single.png"
-          />
-          <Menu
-            active={() => handleClick("Home")}
-            activeState={active}
-            path="/"
-            name="Home"
-          />
-          <Menu
-            active={() => handleClick("Usikkerhet")}
-            activeState={active}
-            path="/riskmatrix"
-            name="Risikomatrise"
-          />
-          <Menu
-            active={() => handleClick("Prosjektstatus")}
-            activeState={active}
-            path="/projectstatus"
-            name="Prosjekt status"
-          />
-          <Menu
-            active={() => handleClick("Page 3")}
-            activeState={active}
-            path="/"
-            name="Page 3"
-          />
-          <Menu
-            active={() => handleClick("Page 4")}
-            activeState={active}
-            path="/"
-            name="Page 4"
-          />
-          <Menu
-            active={() => handleClick("Page 5")}
-            activeState={active}
-            path="/"
-            name="Page 5"
-          />
-        </Flex>
+        <Flex space="between">{setMenu(menu)}</Flex>
         <Divider />
       </div>
     </>
