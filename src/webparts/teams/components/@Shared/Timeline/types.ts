e565275f74c74034b1c121140ebec8004e2a37d8
Martin Ruud <m.ruud93@gmail.com>
2021-03-14 16:09:48 +0100

    Created fetch items, timeline, onclick

--- src/webparts/teams/components/@Shared/Timeline/types.ts
+++ src/webparts/teams/components/@Shared/Timeline/types.ts
@@ -1,7 +1,21 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
-
+import * as moment from 'moment'
 
 export interface ITimelineProps {
-    context: WebPartContext;
+    context?: WebPartContext;
     listName: string;
+}
+
+export interface ITimelineItem {
+    id: number;
+    group: number;
+    title: string;
+    start_time: moment.Moment;
+    end_time: moment.Moment;
+}
+
+export interface ITimelineGroup {
+    id: number;
+    title: string, 
+    height: number;
 }
